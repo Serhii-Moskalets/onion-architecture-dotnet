615353a94096b62d549372f857c1151ea4a17424
Serhii Moskalets <s.mosklaets16@gmail.com>
2025-09-21 22:18:47 +0200

    #1 added privatebuild script with compilation and running of unit tests

---
+++ build.ps1
@@ -0,0 +1,93 @@
+. .\BuildFunctions.ps1
+$startTime = 
+$projectName = "Church Bulletin"
+$base_dir = resolve-path .\
+$source_dir = "$base_dir\src"
+$unitTestProjectPath = "$source_dir\UnitTests"
+$integrationTestProjectPath = "$source_dir\IntegrationTests"
+$projectConfig = $env:BuildConfiguration
+$framework = "net8.0"
+$version = $env:Version
+$verbosity = "m"
+
+$build_dir = "$base_dir\build"
+$test_dir = "$build_dir\test"
+
+if ([string]::IsNullOrEmpty($version)) { $version = "1.0.0.0"}
+if ([string]::IsNullOrEmpty($projectConfig)) {$projectConfig = "Release"}
+ 
+Function Init {
+	rm -r -fo $build_dir -ErrorAction Ignore
+	md $build_dir > $null
+
+	exec {
+		& dotnet clean $source_dir\$projectName.sln -nologo -v $verbosity
+		}
+	exec {
+		& dotnet restore $source_dir\$projectName.sln -nologo --interactive -v $verbosity  
+		}
+    
+
+    Write-Output $projectConfig
+    Write-Output $version
+}
+
+
+Function Compile{
+	exec {
+		& dotnet build $source_dir\$projectName.sln -nologo --no-restore -v `
+			$verbosity -maxcpucount --configuration $projectConfig --no-incremental `
+			/p:TreatWarningsAsErrors="true" `
+			/p:Version=$version /p:Authors="Programming with Palermo" `
+			/p:Product="Church Bulletin"
+	}
+}
+
+Function UnitTests{
+	Push-Location -Path $unitTestProjectPath
+
+	try {
+		exec {
+			& dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura -nologo -v $verbosity --logger:trx `
+			--results-directory $test_dir --no-build `
+			--no-restore --configuration $projectConfig `
+			--collect:"Code Coverage" 
+		}
+	}
+	finally {
+		Pop-Location
+	}
+}
+
+Function IntegrationTest{
+	Push-Location -Path $integrationTestProjectPath
+
+	try {
+		exec {
+			& dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura -nologo -v $verbosity --logger:trx `
+			--results-directory $test_dir --no-build `
+			--no-restore --configuration $projectConfig `
+			--collect:"Code Coverage" 
+		}
+	}
+	finally {
+		Pop-Location
+	}
+}
+
+
+Function PrivateBuild{
+	$projectConfig = "Debug"
+	$sw = [Diagnostics.Stopwatch]::StartNew()
+	Init
+	Compile
+	UnitTests
+	$sw.Stop()
+	write-host "Build time: " $sw.Elapsed.ToString()
+}
+
+Function CIBuild{
+	Init
+	Compile
+	UnitTests
+}
